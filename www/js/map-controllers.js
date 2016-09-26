206f10eae8f6c0e268442dccd6944c6e153ed9df
Erno Mäkinen <ernoma@gmail.com>
2016-09-26 22:57:01 +0300

    Add basic OSM object retrieving and presenting for map center area

--- www/js/map-controllers.js
+++ www/js/map-controllers.js
@@ -1,7 +1,7 @@
 
 var mapControllers = angular.module('mapControllers', [])
 
-.controller('MapCtrl', function($scope, $cordovaGeolocation, leafletData) {
+.controller('MapCtrl', function($scope, $cordovaGeolocation, $http, leafletData) {
 
 	var tilesDict = {
 		openstreetmap: {
@@ -51,7 +51,7 @@ var mapControllers = angular.module('mapControllers', [])
 		  .then(function (position) {
 		    $scope.map.center.lat  = position.coords.latitude;
 		    $scope.map.center.lng = position.coords.longitude;
-		    $scope.map.center.zoom = 15;
+		    $scope.map.center.zoom = 18;
 
 		    $scope.map.markers.now = {
 		      lat:position.coords.latitude,
@@ -67,4 +67,114 @@ var mapControllers = angular.module('mapControllers', [])
 		    console.log(err);
 		  });
 	};
+
+	$scope.overpassResult = "";
+
+	$scope.findOSMObjects = function() {
+
+		var onEachFeature = function (feature, layer) {
+			var content = "";
+
+			content += "<h3>" + feature.properties.type.charAt(0).toUpperCase() + feature.properties.type.slice(1);
+			content += " " + feature.properties.id + "</h3>";
+
+			if (feature.properties.tags != undefined) {
+				content += "<h4>Tags:</h4>";
+				for (var key in feature.properties.tags) {
+						content += key + "=" + feature.properties.tags[key] + "<br>";					
+				}
+			}
+			if (feature.properties.tainted) {
+				content += "<p><b>Note:</b> incomplete geometry</p>";
+			}
+
+			layer.bindPopup(content);
+		}
+
+		var styleFeature = function (feature) {
+
+			var style = {};
+
+			if (feature.properties.type == "way") {
+				if (feature.properties.tags != undefined) {
+					if (feature.properties.tags.wikipedia != undefined) {
+						style.color = '#0f0';
+						console.log(style);
+					}
+					else if (feature.properties.tags.building != undefined) {
+						style.color = '#800000';
+					}
+					else if ((feature.properties.tags.leisure != undefined && feature.properties.tags.leisure == "park") ||
+						(feature.properties.tags.leisure != undefined && feature.properties.tags.leisure == "playground") ||
+						(feature.properties.tags.landuse != undefined && feature.properties.tags.landuse == "grass")
+						) {
+						style.color = '#196619';
+					}
+					else if (feature.properties.tags.highway != undefined ||
+						(feature.properties.tags.amenity != undefined && feature.properties.tags.amenity == "parking")
+						) {
+						style.color = '#4d4d33';
+					}
+					else {
+						style.color = '#404040';
+					}
+				}
+				if (feature.properties.tainted) {
+					style.dashArray = "5, 5";
+					//console.log(style);
+				}
+			}
+
+			return style;
+		}
+
+		var pointToLayer = function (feature, latlng) {
+
+			var geojsonMarkerOptions = {
+			    radius: 12,
+			    fillColor: "#ff7800",
+			    color: "#000",
+			    weight: 1,
+			    opacity: 1,
+			    fillOpacity: 0.4
+			};
+
+			if (feature.properties.tags != undefined) {
+				if (feature.properties.tags.wikipedia != undefined) {
+					geojsonMarkerOptions.fillColor = "#0f0";
+				}
+			}
+
+			return L.circleMarker(latlng, geojsonMarkerOptions);
+		}
+
+
+		data = "[out:json];(node(around:100.0," + $scope.map.center.lat + "," + $scope.map.center.lng + ");<;);out meta;";
+		$http.get('http://www.overpass-api.de/api/interpreter?data=' + data)
+			.success(function(data, status, headers,config){
+		      console.log('data success');
+		      //console.log(data); // for browser console
+		      //$scope.overpassResult = data; // for UI
+
+		      var geoJson = osmtogeojson(data);
+		      console.log(geoJson);
+
+		      leafletData.getMap().then(function(map) {
+		      	L.geoJson(geoJson, {
+		      		style: styleFeature,
+		      		pointToLayer: pointToLayer,
+		      		onEachFeature: onEachFeature
+		      	}).addTo(map);
+		      });
+		    })
+		    .error(function(data, status, headers,config){
+		      console.log('data error');
+		    })
+		    .then(function(result){
+		      //things = result.data;
+		    });
+
+		// (node(around:100.0,61.496507,23.781377);<;);out meta; // sorsapuisto
+		// (node(around:100.0,61.5,23.766667);<;);out meta; // koskipuisto
+	}
 });
