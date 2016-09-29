6eb64a2f46018a5bb8a0662dba6451e403ea0635
Erno Mäkinen <ernoma@gmail.com>
2016-09-29 22:30:51 +0300

    Continue implementing Mediawiki detail views

--- www/js/wiki-controllers.js
+++ www/js/wiki-controllers.js
@@ -53,13 +53,22 @@ var wikiControllers = angular.module('wikiControllers', [])
 		    if ($scope.formData.site == "wikidata") {
 			    for (var i = 0; i < data.search.length; i++) {
 			    	$scope.searchResults.push({
-			    		id: data.search[i].id,
+			    		id: $scope.formData.language + $scope.formData.site + "+" + data.search[i].id,
 			    		label: data.search[i].label + " (" + data.search[i].id + ")",
 			    		description: data.search[i].description
 			    	});
 			    }
 			}
-			else {
+			else if ($scope.formData.site == "commons") {
+				for (var i = 0; i < data[1].length; i++) {
+					$scope.searchResults.push({
+						id: $scope.formData.language + $scope.formData.site + "+" + data[1][i].replace(/\s+/g, '_'),
+						label: data[1][i],
+						description: data[3][i]
+					});
+				}
+			}
+			else { // wikipedia
 				for (var i = 0; i < data[1].length; i++) {
 					$scope.searchResults.push({
 						id: $scope.formData.language + $scope.formData.site + "+" + data[1][i].replace(/\s+/g, '_'),
@@ -78,6 +87,10 @@ var wikiControllers = angular.module('wikiControllers', [])
 
 .controller('WikiDetailCtrl', function($scope, $stateParams, $http) {
 		console.log($stateParams.wikiId);
+
+		$scope.title = "";
+		$scope.siteURL = "";
+
 		var parts = $stateParams.wikiId.split("+");
 		var lang = parts[0].substring(0, 2);
 		var site = parts[0].substring(2);
@@ -87,56 +100,61 @@ var wikiControllers = angular.module('wikiControllers', [])
 		console.log(site);
 		console.log(id);
 
-		var url = "https://";
 		if (site == "wikipedia") {
-			url += lang;
-			url += ".wikipedia.org/w/api.php?action=query";
-			url += "&titles=" + id;
-			url += "&prop=revisions&rvprop=content";
-			url += "&callback=JSON_CALLBACK&format=json";
-
-			$http.jsonp(url).
-			  success(function(data) {
-			    console.log(data);
-
-			  })
-			  .error(function (data) {
-			  	console.log('data error');
-			    console.log(data);
-			  });
+			// TODO there are many ways to go to retrieve and present (and edit) the data
+			// var url = "https://";
+			// url += lang;
+			// url += ".wikipedia.org/w/api.php?action=query";
+			// url += "&titles=" + id;
+			// url += "&prop=revisions&rvprop=content";
+			// url += "&callback=JSON_CALLBACK&format=json";
+			//
+			// $http.jsonp(url).
+			//   success(function(data) {
+			//     console.log(data);
+			//
+			//   })
+			//   .error(function (data) {
+			//   	console.log('data error');
+			//     console.log(data);
+			//   });
 		}
 		else if (site == "wikidata") {
 			//TODO
-			// url = wdk.searchEntities({
-			// 	search: $scope.formData.searchText,
-			// 	language: $scope.formData.language,
-			// 	limit: 50
-			// 	});
-			//		url += "&callback=JSON_CALLBACK";
-			// $http.jsonp(url).
-			// 	success(function(data) {
-			// 		console.log(data);
-			//
-			// 	})
-			// 	.error(function (data) {
-			// 		console.log('data error');
-			// 		console.log(data);
-			// 	});
+
+			var url = wdk.getEntities(id);
+
+			url += "&callback=JSON_CALLBACK";
+
+			$http.jsonp(url).
+				success(function(data) {
+					console.log(data);
+					var simplifiedClaims = wdk.simplifyClaims(data.entities[id].claims);
+					console.log(simplifiedClaims);
+				})
+				.error(function (data) {
+					console.log('data error');
+					console.log(data);
+				});
 		}
 		else { // site == commons
-			//TODO
-			// url += "commons.wikimedia.org/w/api.php?action=opensearch";
-			// url += "&limit=" + 50;
-			// url += "&search=" + $scope.formData.searchText;
-			// url += "&callback=JSON_CALLBACK";
-			// $http.jsonp(url).
-			// 	success(function(data) {
-			// 		console.log(data);
-			//
-			// 	})
-			// 	.error(function (data) {
-			// 		console.log('data error');
-			// 		console.log(data);
-			// 	});
+			//TODO show page link and image thumbnails with links
+
+			$scope.title = id.replace(/_+/g, ' ');
+
+			$scope.siteURL = "https://commons.wikimedia.org/wiki/" + id;
+
+			var url = "https://commons.wikimedia.org/w/api.php?callback=JSON_CALLBACK&action=query&prop=images&imlimit=50&format=json&titles="
+			url += id;
+
+			$http.jsonp(url).
+				success(function(data) {
+					console.log(data);
+
+				})
+				.error(function (data) {
+					console.log('data error');
+					console.log(data);
+				});
 		}
 });
