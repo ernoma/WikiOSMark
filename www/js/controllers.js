dc943559523638d6b92d5b10d943071911951aa6
Erno Mäkinen <ernoma@gmail.com>
2016-10-13 12:48:03 +0300

    Move OSM Object info menu functions from MapCtrl to TabCtrl

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -54,7 +54,7 @@ angular.module('starter.controllers', [])
   });
 })
 
-.controller('TabCtrl', function($scope, $rootScope, $cordovaCamera, GeoLocation, PhotoGallery) {
+.controller('TabCtrl', function($scope, $rootScope, $ionicPopup, $cordovaCamera, GeoLocation, PhotoGallery, Wiki, OpenStreetMap, AppSettings) {
   $scope.findOSMObjects = function() {
     //console.log("in TabCtrl");
     $scope.$broadcast('findOSMObjects');
@@ -64,24 +64,211 @@ angular.module('starter.controllers', [])
     $scope.$broadcast('locateMe');
   }
 
+  $scope.showOSMObjectPage = function() {
+    window.open("https://www.openstreetmap.org/" +
+      $scope.osmObjectInfo.type + "/" +
+      $scope.osmObjectInfo.id,
+      '_system', 'location=yes');
+  }
+
   $scope.inputWikidataChange = function() {
-    $scope.$broadcast('inputWikidataChange');
-  }
-  $scope.selectWikidataSearchResult = function(item) {
-    $scope.$broadcast('selectWikidataSearchResult', item);
-  }
-  $scope.inputWikipediaChange = function() {
-    $scope.$broadcast('inputWikipediaChange');
-  }
-  $scope.selectWikipediaSearchResult = function(item) {
-    $scope.$broadcast('selectWikipediaSearchResult', item);
-  }
-  $scope.inputCommonsChange = function() {
-    $scope.$broadcast('inputCommonsChange');
-  }
-  $scope.selectCommonsSearchResult = function(item) {
-    $scope.$broadcast('selectCommonsSearchResult', item);
-  }
+		// TODO search and suggest options when 2 or more characters in the input field
+		$scope.mapControllerData.changesToSave = true;
+		if ($scope.osmObjectInfo.wikidataTag.length >= 2) {
+			Wiki.queryMediaWiki("wikidata", AppSettings.getDefaultLanguage(), $scope.osmObjectInfo.wikidataTag, function(data) {
+					//console.log(data);
+					$scope.searchResults.wikidataResults = data.search;
+			});
+		}
+		else if ($scope.osmObjectInfo.wikidataTag.length == 0) {
+			$scope.searchResults.wikidataResults = "";
+		}
+	}
+
+	$scope.inputWikipediaChange = function() {
+		// TODO search and suggest options when enought characters in the input field
+		$scope.mapControllerData.changesToSave = true;
+		if ($scope.osmObjectInfo.wikipediaTag.includes(":")) {
+			if ($scope.osmObjectInfo.wikipediaTag.length >= 6) {
+				var parts = $scope.osmObjectInfo.wikipediaTag.split(":");
+				if (parts.length > 1) {
+					Wiki.queryMediaWiki("wikipedia", parts[0], parts[1], function(data) {
+							//console.log(data);
+							$scope.searchResults.wikipediaResults = data[1];
+					});
+				}
+			}
+		}
+		else if ($scope.osmObjectInfo.wikipediaTag.length >= 3) {
+			Wiki.queryMediaWiki("wikipedia", AppSettings.getDefaultLanguage(), $scope.osmObjectInfo.wikipediaTag, function(data) {
+					//console.log(data);
+					$scope.searchResults.wikipediaResults = data[1];
+			});
+		}
+		else if ($scope.osmObjectInfo.wikipediaTag.length == 0) {
+			$scope.searchResults.wikipediaResults = "";
+		}
+	}
+
+	$scope.inputCommonsChange = function() {
+		// TODO search and suggest options when 3 or more characters in the input field
+		$scope.mapControllerData.changesToSave = true;
+		$scope.commonsResults = null;
+		if ($scope.osmObjectInfo.wikimediaCommonsTag.length >= 3) {
+			Wiki.queryMediaWiki("commons", AppSettings.getDefaultLanguage(), $scope.osmObjectInfo.wikimediaCommonsTag, function(data) {
+					console.log(data);
+					$scope.searchResults.commonsResults = data[1];
+			});
+		}
+		else if ($scope.osmObjectInfo.wikimediaCommonsTag.length == 0) {
+			$scope.searchResults.commonsResults = "";
+		}
+	}
+
+	$scope.selectWikidataSearchResult = function(item) {
+		$scope.osmObjectInfo.wikidataTag = item.label + " (" + item.id + ")";
+		$scope.searchResults.wikidataResults = null;
+		// TODO: update wikipedia and commons tags if they can be found via Wiki search
+	}
+
+	$scope.selectWikipediaSearchResult = function(item) {
+		if ($scope.osmObjectInfo.wikipediaTag.includes(":") && !item.includes(":")) {
+			$scope.osmObjectInfo.wikipediaTag = $scope.osmObjectInfo.wikipediaTag.split(":")[0] + ":" + item;
+		}
+		else {
+			$scope.osmObjectInfo.wikipediaTag = item;
+		}
+		$scope.searchResults.wikipediaResults = null;
+		// TODO: update wikidata tag if it can be found via Wiki search
+	}
+
+	$scope.selectCommonsSearchResult = function(item) {
+		$scope.osmObjectInfo.wikimediaCommonsTag = item;
+		$scope.searchResults.commonsResults = null;
+	}
+
+  $scope.showWikidataPage = function() {
+		console.log("in showWikidataPage");
+		var item = $scope.osmObjectInfo.wikidataTag;
+		if ($scope.osmObjectInfo.wikidataTag.includes("(")) {
+			var temp = $scope.osmObjectInfo.wikidataTag.split("(")[1];
+			item = temp.split(")")[0];
+		}
+		Wiki.showWikidataPage(item);
+	}
+	$scope.showWikipediaPage = function() {
+		console.log("in showWikipediaPage");
+		var item = $scope.osmObjectInfo.wikipediaTag;
+		if (!item.includes(":")) {
+			item = AppSettings.getDefaultLanguage() + ":" + $scope.osmObjectInfo.wikipediaTag;
+		}
+		Wiki.showWikipediaPage(item);
+	}
+	$scope.showCommonsPage = function() {
+		console.log("in showCommonsPage");
+		Wiki.showWikimediaCommonsPage($scope.osmObjectInfo.wikimediaCommonsTag);
+	}
+
+  $scope.saveWikiTagChanges = function() {
+		// TODO somehow ensure that user is logged in before trying to save changes
+
+		// Check
+		// 1. if there are changes to the wiki item(s) and if there are
+		// 2. verify the changeset comment from the user,
+		// 3. call OpenStreetMapService save accordingly ("trim" the UI input values)
+
+		//console.log($scope);
+		//console.log($scope.mapControllerData.selectedFeature);
+		//console.log($scope.osmObjectInfo.wikidataTag);
+		//console.log($scope.osmObjectInfo.wikipediaTag);
+		//console.log($scope.osmObjectInfo.wikimediaCommonsTag);
+
+		var changesetCommentPopup = $ionicPopup.show({
+	    template: '<input type="text" ng-model="mapControllerData.changesetComment">',
+	    title: 'Changeset comment',
+	    scope: $scope,
+	    buttons: [
+	      { text: 'Cancel',
+					onTap: function(e) {
+						return false;
+					}
+				},
+	      {
+	        text: '<b>Save</b>',
+	        type: 'button-positive',
+	        onTap: function(e) {
+	            return true;
+	          }
+	      }
+	    ]
+	  });
+
+		var feature = $scope.mapControllerData.selectedFeature;
+
+		var tags = {
+			wikidata: null,
+			wikipedia: null,
+			wikimedia_commons: null,
+		};
+
+		$scope.mapControllerData.changesetComment = "Modified ";
+		var changeCount = 0;
+
+		if (feature.properties.tags.wikidata != undefined) {
+			tags.wikidata = feature.properties.tags.wikidata;
+		}
+		if (feature.properties.tags.wikipedia != undefined) {
+			tags.wikipedia = feature.properties.tags.wikipedia;
+		}
+		if (feature.properties.tags.wikimediaCommonsTag != undefined) {
+			tags.wikimediaCommonsTag = feature.properties.tags.wikimediaCommonsTag;
+		}
+
+		var item = $scope.osmObjectInfo.wikidataTag;
+		if (item != null && item.includes("(")) {
+			var temp = $scope.osmObjectInfo.wikidataTag.split("(")[1];
+			item = temp.split(")")[0];
+		}
+		if (item != feature.properties.tags.wikidata) {
+			tags.wikidata = item;
+			$scope.mapControllerData.changesetComment += "wikidata";
+			changeCount++;
+		}
+
+		var item = $scope.osmObjectInfo.wikipediaTag;
+		if (item != null && item != "" && !item.includes(":")) {
+			item = AppSettings.getDefaultLanguage() + ":" + $scope.osmObjectInfo.wikipediaTag;
+		}
+		if (item != feature.properties.tags.wikipedia) {
+			tags.wikipedia = item;
+			if (changeCount > 0) {
+				$scope.mapControllerData.changesetComment += ", ";
+			}
+			$scope.mapControllerData.changesetComment += "wikipedia";
+			changeCount++;
+		}
+
+		var item = $scope.osmObjectInfo.wikimediaCommonsTag;
+		if (item != feature.properties.tags.wikimediaCommonsTag) {
+			tags.wikimedia_commons = $scope.osmObjectInfo.wikimediaCommonsTag;
+			if (changeCount > 0) {
+				$scope.mapControllerData.changesetComment += " and ";
+			}
+			$scope.mapControllerData.changesetComment += "wikimedia_commons";
+			changeCount++;
+		}
+
+		$scope.mapControllerData.changesetComment += (changeCount > 1  ? " tags." : " tag.");
+
+		changesetCommentPopup.then(function(res) {
+			console.log(res);
+			if (res == true) {
+				//OpenStreetMap.getPermissions();
+				OpenStreetMap.updateElementTags($scope.mapControllerData.selectedFeature, $scope.mapControllerData.changesetComment, tags);
+		 	 	$scope.mapControllerData.changesToSave = false;
+			}
+	  });
+	}
 
   $scope.takePhoto = function() {
     var options = {
